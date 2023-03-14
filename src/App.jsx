afaf8e4c94b12aa72eecc77d86a4d95a77558245
Anastasia Smirnova <smirnova.anastasia.k@gmail.com>
2023-03-14 15:37:28 +0200

    tailwind_migration_1

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { createContext, useState } from "react";
+import Header from "./components/Header";
+
+import "./App.css";
+
+//Create context
+export const inputContext = createContext();
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  console.log(inputValue);
+
+  const value = {
+    inputValue,
+    setInputValue,
+  };
+
+  return (
+    <inputContext.Provider value={value}>
+      <div classNaame="App">
+        <Header />
+      </div>
+    </inputContext.Provider>
+  );
+}
+
+export default App;
